80d91a7ac69756fa34b1b2f65911a702aad5407a
FishingHacks <thejominingcraft@gmail.com>
2022-05-22 16:26:46 +0200

    Added bin-file to run a lightframe app

---
+++ bin/index.js
@@ -0,0 +1,27 @@
+const { join } = require("path");
+
+if (process.argv.length < 3) {
+    console.log("<> - Required | [] - Optional");
+    console.log("---");
+    console.log("Usage: lightframe <projectdirectory> [port]");
+    console.log("lightframe -v");
+    process.exit(1);
+}
+
+if(process.argv[2] == "-v" || process.argv[2] == "--version") {
+    console.log("lightframe v" + require(join(process.argv[1], "../../package.json")).version);
+    process.exit(0);
+}
+
+const path = join(process.cwd(), process.argv[2]);
+const port = (process.argv[3] || 8080);
+
+const express = require("express")();
+
+const { loadApp } = require(join(process.argv[1], "../../framework/index.js"));
+
+(async function() {
+    await loadApp(path, express);
+
+    express.listen(port, ()=>console.log(`Listening on http://localhost:${port}`));
+})();
